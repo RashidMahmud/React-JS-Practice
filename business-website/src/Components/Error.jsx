0b0448fc2cd6f5abf37a5e51f35365b38ab3fa7c
Rashid Mahmud <rashidmahmud999@gmail.com>
2026-01-22 21:48:59 +0600

    useNavigate

--- business-website/src/Components/Error.jsx
+++ business-website/src/Components/Error.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 
 function Error() {
+  let Navigate = useNavigate()
+  const handleClick = () => {
+  Navigate("/")
+  }
   return (
     <div>
         <h1 className='mt-30 text-center'>This Page Is Not Exist, Please Return Home</h1>
         <div className='w-full'>
-            <button className='btn bg-indigo-500 p-2 rounded-md mt-35 text-white mx-auto block cursor-pointer '>Back Home</button>
+            <button className='btn bg-indigo-500 p-2 rounded-md mt-35 text-white mx-auto block cursor-pointer' onClick={handleClick}>Back Home</button>
         </div>
     </div>
   )
