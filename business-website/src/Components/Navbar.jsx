6c0097c3d42ffec25095e7f010dbdf633aab2a0d
Rashid Mahmud <rashidmahmud999@gmail.com>
2026-01-22 22:42:35 +0600

    Nav Active Link

--- business-website/src/Components/Navbar.jsx
+++ business-website/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 function Navbar() {
   return (
     <>
@@ -7,10 +7,10 @@ function Navbar() {
         <ul className='max-w-[90vw] flex flex-wrap justify-between mx-auto'>
             <a href=""className='font-semibold text-2xl'>Dev-Rashid</a>
             <div className='flex [&>li]:pl-7'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="About">About</Link></li>
-                <li><Link to="Contact">Contact</Link></li>
-                <li><Link to="Signup">Signup</Link></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="About">About</NavLink></li>
+                <li><NavLink to="Contact">Contact</NavLink></li>
+                <li><NavLink to="Signup">Signup</NavLink></li>
             </div>
         </ul>
     </nav>
